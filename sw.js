a7e4221f61fa9ffa69403e8a309975f7c56eafe1
Simon <simon@gripencreutz.com>
2019-10-28 12:24:59 +0100

    reverting back

--- sw.js
+++ sw.js
@@ -1,39 +1,39 @@
-const staticCacheName = 'cv-cache'
-// const dynamicCache = 'dynamic-v2'
+// const staticCacheName = 'cv-cache'
+// // const dynamicCache = 'dynamic-v2'
 
-const assets = [
-    '/',
-    '/index.html',
-    '/images/cloud.png',
-    '/images/mountain.png',
-    '/images/simon.jpg',
-    '/CSS/style.css',
-]
+// const assets = [
+//     '/',
+//     '/index.html',
+//     '/images/cloud.png',
+//     '/images/mountain.png',
+//     '/images/simon.jpg',
+//     '/CSS/style.css',
+// ]
 
-self.addEventListener('install', evt => {
-    //console.log('Service worker installed')
-    evt.waitUntil(
-        caches.open(staticCacheName).then(cache => {
+// self.addEventListener('install', evt => {
+//     //console.log('Service worker installed')
+//     evt.waitUntil(
+//         caches.open(staticCacheName).then(cache => {
 
-            cache.addAll(assets)
-            console.log(' Caching shell assets')
-        }))
-})
+//             cache.addAll(assets)
+//             console.log(' Caching shell assets')
+//         }))
+// })
 
-self.addEventListener('fetch', evt => {
-    //console.log('fetch event', evt)
-    evt.respondWith(
-        caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request)
-                .then(fetchRes => {
-                    return caches.open(dynamicCache).then(cache => {
-                        cache.put(evt.request.utl, fetchRes.clone())
-                        return fetchRes
-                    })
-                })
-        })
-    )
-})
+// self.addEventListener('fetch', evt => {
+//     //console.log('fetch event', evt)
+//     evt.respondWith(
+//         caches.match(evt.request).then(cacheRes => {
+//             return cacheRes || fetch(evt.request)
+//                 .then(fetchRes => {
+//                     return caches.open(dynamicCache).then(cache => {
+//                         cache.put(evt.request.utl, fetchRes.clone())
+//                         return fetchRes
+//                     })
+//                 })
+//         })
+//     )
+// })
 
 // self.addEventListener('activate', evt => {
 //     //console.log('Service worker activated')
