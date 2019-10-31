552295adaad4e03aaff5fa12a050f472614e56c8
Simon <simon@gripencreutz.com>
2019-10-31 14:02:42 +0100

    Pwa update

--- sw.js
+++ sw.js
@@ -1,49 +1,38 @@
-// const staticCacheName = 'cv-cache'
-// // const dynamicCache = 'dynamic-v2'
+const staticCache = 'site-static-v1'
 
-// const assets = [
-//     '/',
-//     '/index.html',
-//     '/images/cloud.png',
-//     '/images/mountain.png',
-//     '/images/simon.jpg',
-//     '/CSS/style.css',
-// ]
+// install event
+const assets = [
+    '/',
+    '/index.html',
+    '/CSS/style.css',
+    '/images/cloud.png',
+    '/images/mountain.png',
+    '/images/simon.jpg',
+    'https://fonts.googleapis.com/css?family=Merriweather|Montserrat|Sacramento&display=swap',
+    'https://media.giphy.com/media/dscTJjpsiVamjIk6nk/giphy.gif',
+    'https://media2.giphy.com/media/eJGAjgTxQkdf6sKXhu/giphy.gif?cid=790b76115d4839ce7444744c496fb582&rid=giphy.gif'
+]
+self.addEventListener('install', evt => {
+    //console.log('service worker has been installed')
+    evt.waitUntil(
+        caches.open(staticCache).then(cache => {
+            console.log('adding assets')
+            cache.addAll(assets)
+        })
+    )
+})
 
-// self.addEventListener('install', evt => {
-//     //console.log('Service worker installed')
-//     evt.waitUntil(
-//         caches.open(staticCacheName).then(cache => {
+//activate event
+self.addEventListener('activate', evt => {
+    console.log('service worker has been activated')
+})
 
-//             cache.addAll(assets)
-//             console.log(' Caching shell assets')
-//         }))
-// })
-
-// self.addEventListener('fetch', evt => {
-//     //console.log('fetch event', evt)
-//     evt.respondWith(
-//         caches.match(evt.request).then(cacheRes => {
-//             return cacheRes || fetch(evt.request)
-//                 .then(fetchRes => {
-//                     return caches.open(dynamicCache).then(cache => {
-//                         cache.put(evt.request.utl, fetchRes.clone())
-//                         return fetchRes
-//                     })
-//                 })
-//         })
-//     )
-// })
-
-// self.addEventListener('activate', evt => {
-//     //console.log('Service worker activated')
-//     evt.waitUntil(
-//         caches.keys().then(keys => {
-//             //console.log(keys)
-//             return Promise.all(keys
-//                 .filter(key => key !== staticCacheName)
-//                 .map(key => caches.delete())
-//             )
-//         })
-//     )
-// })
+// fetch event
+self.addEventListener('fetch', evt => {
+    //console.log('Fetch event', evt)
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    )
+})
